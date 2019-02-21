ca2165fd5b1b76bca369385e597cb7b06e08b35f
Rafael <220rrs@gmail.com>
2019-02-21 16:47:05 +0000

    Updates

---
+++ precache-manifest.6a46270ba8f4951975d36c25f8927028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b714b3e93ac184ff4f35",
+    "url": "/react-countries/static/js/runtime~main.b714b3e9.js"
+  },
+  {
+    "revision": "f912c73d3e983a6951c4",
+    "url": "/react-countries/static/js/main.f912c73d.chunk.js"
+  },
+  {
+    "revision": "c13f52a9c6f9ff95701e",
+    "url": "/react-countries/static/js/2.c13f52a9.chunk.js"
+  },
+  {
+    "revision": "f912c73d3e983a6951c4",
+    "url": "/react-countries/static/css/main.5fe79034.chunk.css"
+  },
+  {
+    "revision": "9d612626cd49b6961b2e130e4bd06bf7",
+    "url": "/react-countries/index.html"
+  }
+];
