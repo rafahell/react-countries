a75c80d221b83d08f7f4c7c9574827e70ac13dea
Rafael <220rrs@gmail.com>
2019-02-22 10:25:40 +0000

    gh page issue

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render( <BrowserRouter>
+ReactDOM.render( <BrowserRouter basename="/">
 <App />
 </BrowserRouter>, document.getElementById('root'));
 
